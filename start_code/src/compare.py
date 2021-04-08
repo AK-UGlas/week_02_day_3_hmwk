f8ce378ea3ddd3ffe5a6ee77c28f984f8c81a79b
Allen_K <allenkelly278@gmail.com>
2021-04-08 08:48:13 +0100

    all tests working

---
+++ start_code/src/compare.py
@@ -0,0 +1,6 @@
+def compare(first, second):
+    if first < second:
+        return f"{first} is less than {second}"
+    elif first == second:
+        return "numbers are equal"
+    return f"{first} is greater than {second}"
